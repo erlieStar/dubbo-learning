0b39db76c8cfca8f3d8c42029719683255e4e724
lilimin <290059742@qq.com>
2020-04-13 20:31:40 +0800

    1.重置依赖，增加配置

--- producer/src/main/java/com/javashitang/producer/service/UserServiceImpl.java
+++ producer/src/main/java/com/javashitang/producer/service/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.javashitang.producer.service;
 
 import com.javashitang.api.pojo.UserInfo;
 import com.javashitang.api.service.UserService;
+import org.apache.dubbo.config.annotation.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Override
