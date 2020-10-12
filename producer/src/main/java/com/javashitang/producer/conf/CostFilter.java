f60d8e8c4c000c6f62bc8de414743bd558e42fc4
lilimin <290059742@qq.com>
2020-10-12 17:42:05 +0800

    增加filter

---
+++ producer/src/main/java/com/javashitang/producer/conf/CostFilter.java
@@ -0,0 +1,29 @@
+package com.javashitang.producer.conf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
+
+import static org.apache.dubbo.monitor.MonitorService.PROVIDER;
+
+/**
+ * @author lilimin
+ * @since 2020-10-12
+ */
+@Slf4j
+@Activate(group = PROVIDER)
+public class CostFilter implements Filter {
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        long start = System.currentTimeMillis();
+        Result result = invoker.invoke(invocation);
+        long cost = System.currentTimeMillis() - start;
+        log.info("request cost:[" + invocation.getMethodName() + "]" + cost);
+        return result;
+    }
+}
