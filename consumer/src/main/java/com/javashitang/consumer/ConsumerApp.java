f3c3408d7b7539202fb058bdb3a3e8d00495bda2
lilimin <290059742@qq.com>
2021-06-04 09:50:18 +0800

    去掉无用类

--- consumer/src/main/java/com/javashitang/consumer/ConsumerApp.java
+++ consumer/src/main/java/com/javashitang/consumer/ConsumerApp.java
@@ -1,7 +1,6 @@
 package com.javashitang.consumer;
 
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
